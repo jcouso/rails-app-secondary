ff40dd29fc2c0512db2d3e280474cb5105659c2f
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-15 19:26:17 -0300

    first commit admin securities controller views

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -2,4 +2,5 @@ class SecuritiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :search]
   def index
   end
+
 end
--- app/policies/security_policy.rb
+++ app/policies/security_policy.rb
@@ -1,7 +1,33 @@
 class SecurityPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
+  end
+
+  def create?
+    user_is_owner?
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  def edit?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record_user == user
   end
 end
