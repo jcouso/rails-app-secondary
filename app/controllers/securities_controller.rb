723f139e645993b6be67336c17be62cd997ea198
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-21 16:34:45 -0300

    Inline form + full width banner + datepicker

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -1,7 +1,8 @@
 class SecuritiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :search]
   before_action :set_security, only: [:show]
-  layout "landing-page", only: [ :index ]
+  layout "landing-page", only: [:index, :search]
+
   def index
     @securities = policy_scope(Security)
   end
@@ -16,17 +17,31 @@ class SecuritiesController < ApplicationController
     @search = Search.new
 
     if params[:search].present?
+      @search.security_type_id = params[:search][:security_type_id]
       @search.price = params[:search][:price]
       @search.maturity = params[:search][:maturity]
+      @search.issuer_id = params[:search][:issuer_id]
+      @search.rate = params[:search][:rate]
+    end
+
+    if @search.security_type_id.present?
+      @securities = @securities.where(security_type_id: @search.security_type_id)
     end
 
     if @search.maturity.present?
-      date = @search.maturity + "-12-31"
-      @securities = @securities.where("maturity <= ?", date)
+      @securities = @securities.where("maturity <= ?", @search.maturity)
     end
 
     if @search.price.present?
       @securities = @securities.where("unit_price * quantity <= ?", @search.price)
+    end
+
+    if @search.issuer_id.present?
+      @securities = @securities.where(issuer_id: @search.issuer_id)
+    end
+
+    if @search.rate.present?
+      @securities =@securities.where("rate >=?", @search.rate)
     end
   end
 
@@ -36,6 +51,5 @@ class SecuritiesController < ApplicationController
     def set_security
         @security = Security.find(params[:id])
         authorize @security
-
-  end
+    end
 end
--- app/models/concerns/search.rb
+++ app/models/concerns/search.rb
@@ -1,5 +1,5 @@
 class Search
   include ActiveModel::Model
 
-  attr_accessor :price, :maturity
+  attr_accessor :price, :maturity, :issuer_id, :rate, :indexer, :security_type_id
 end
