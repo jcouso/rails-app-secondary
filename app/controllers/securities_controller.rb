19a1a466c3451589848f209e6a05cfbd2db713c3
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-21 18:40:19 -0300

    search ok

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -14,7 +14,7 @@ class SecuritiesController < ApplicationController
   def search
     @securities = policy_scope(Security).order("created_at DESC")
     authorize @securities
-    @search = Search.new
+    @search = Search.new(search_params)
 
     if params[:search].present?
       @search.security_type_id = params[:search][:security_type_id]
@@ -29,11 +29,11 @@ class SecuritiesController < ApplicationController
     end
 
     if @search.maturity.present?
-      @securities = @securities.where("maturity <= ?", @search.maturity)
+      @securities = @securities.where("maturity <= ?", @search.maturity).reorder("maturity DESC")
     end
 
     if @search.price.present?
-      @securities = @securities.where("unit_price * quantity <= ?", @search.price)
+      @securities = @securities.where("unit_price * quantity <= ?", @search.price).reorder("price DESC")
     end
 
     if @search.issuer_id.present?
@@ -41,7 +41,7 @@ class SecuritiesController < ApplicationController
     end
 
     if @search.rate.present?
-      @securities =@securities.where("rate >=?", @search.rate)
+      @securities =@securities.where("rate >=?", @search.rate).reorder("rate DESC")
     end
   end
 
@@ -51,5 +51,9 @@ class SecuritiesController < ApplicationController
     def set_security
         @security = Security.find(params[:id])
         authorize @security
+    end
+
+    def search_params
+      params.require(:search).permit(:price, :maturity, :issuer_id, :rate, :indexer, :security_type_id)
     end
 end
--- app/models/concerns/search.rb
+++ app/models/concerns/search.rb
@@ -2,4 +2,12 @@ class Search
   include ActiveModel::Model
 
   attr_accessor :price, :maturity, :issuer_id, :rate, :indexer, :security_type_id
+
+  def empty?
+    !present?
+  end
+
+  def present?
+    price.present? || maturity.present? || issuer_id.present? || rate.present? || indexer.present? || security_type_id.present?
+  end
 end
