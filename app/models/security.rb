76f88723fadfc48c72bb9875cf925a33ecfb59c4
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-22 18:38:01 -0300

    included issue-date

--- app/models/security.rb
+++ app/models/security.rb
@@ -14,7 +14,7 @@ class Security < ApplicationRecord
   #validates :rate, presence: true, numericality: true
   #validates :indexer, presence: true
   #validates :unit_price, presence: true, numericality: true
-
+  #validates :issue_date, presence: true, date: true
   validate :maturity_cannot_be_in_the_past, on: :create
   validate :date_limit_cannot_be_in_the_past, on: :create
   validate :validate_date_limit_before_maturity, on: :create
@@ -28,13 +28,21 @@ class Security < ApplicationRecord
     (self.rate*100).round(2)
   end
 
-  def future_value
-    value * (rate ** )
+  def time_elapsed
+    (Time.current - self.issue_date).to_i
   end
 
-  def present_value
-    future_value / (rate **)
+  def future_value
+    n = time_elapsed * (360.to_f/365)
+    pv = self.value
+    i_dia = ((self.rate.to_f / 100)+1)**(1.to_f/360)
+    fv = pv.to_f * ((i_dia.to_f)**(n.to_f))
+    fv
   end
+
+  # def present_value
+  #   future_value / (rate **)
+  # end
 
   private
 
@@ -53,6 +61,12 @@ class Security < ApplicationRecord
   def validate_date_limit_before_maturity
     if date_limit > maturity
       errors.add(:date_limit, "Título não pode ser aberto para negociação após data de vencimento")
+    end
+  end
+
+  def maturity_cannot_be_before_issue_date
+    if maturity < issue_date
+      errors.add(:maturity, "A data de vencimento tem que ser depois da data de emissão")
     end
   end
 end
