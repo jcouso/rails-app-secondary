75dd46bf3b3a882c4324d8f573670d58e5a0a61b
Lucas Mussi <lucasmussi@gmail.com>
2017-08-23 15:35:49 -0300

    indexer added to bids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822165920) do
+ActiveRecord::Schema.define(version: 20170823182946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20170822165920) do
     t.string   "account_agency"
     t.string   "account_number"
     t.date     "expedition_date"
-    t.float    "balance", default: 0
+    t.float    "balance"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
