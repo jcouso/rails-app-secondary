f22c3824bcb47e059ffec259f4c7f47b8d01f886
Juan Couso <juan.couso@gmail.com>
2017-08-23 17:57:18 -0300

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -69,18 +69,18 @@ ActiveRecord::Schema.define(version: 20170823190137) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",  null: false
+    t.string   "encrypted_password",     default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "name"
     t.string   "cpf"
     t.string   "document_number"
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 20170823190137) do
     t.string   "account_agency"
     t.string   "account_number"
     t.date     "expedition_date"
-    t.float    "balance",               default: 0, null: false
+    t.float    "balance",                default: 0.0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
