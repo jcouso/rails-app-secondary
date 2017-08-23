923b3a09df6392d7dfdf07c127bed7f45c205a4c
Lucas Mussi <lucasmussi@gmail.com>
2017-08-23 11:02:44 -0300

    minor changes in migrations

--- db/migrate/20170815133550_create_securities.rb
+++ db/migrate/20170815133550_create_securities.rb
@@ -4,7 +4,7 @@ class CreateSecurities < ActiveRecord::Migration[5.0]
       t.references :user, foreign_key: true
       t.references :issuer, foreign_key: true
       t.references :security_type, foreign_key: true
-      t.string :mode
+      t.string :mode, default: "Leilão"
       t.string :name
       t.string :code
       t.date :maturity
--- db/migrate/20170822165920_add_balance_to_users.rb
+++ db/migrate/20170822165920_add_balance_to_users.rb
@@ -1,5 +1,5 @@
 class AddBalanceToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :balance, :float
+    add_column :users, :balance, :float, default: 0.0
   end
 end
