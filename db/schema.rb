014debd4be75c35ddfe9ff7414f26c55a7dbcb47
Fernando Antunes <fernandoantunes@gmail.com>
2017-08-24 11:02:08 -0300

    Rating field included in Issuer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823190137) do
+ActiveRecord::Schema.define(version: 20170824135924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170823190137) do
     t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "rating"
   end
 
   create_table "securities", force: :cascade do |t|
