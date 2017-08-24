76fc11280c0675dd675be827bf1292dd292d9550
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-24 18:53:41 -0300

    current value

---
+++ app/assets/javascripts/calc_current_value.js
@@ -0,0 +1,48 @@
+$(function() {
+  $("[name='security[issue_date]']").on("change", function() {
+    checkValuesPresence()
+  });
+  $("[name='security[maturity]']").on("change", function() {
+    checkValuesPresence()
+  });
+  $("[name='security[quantity]']").on("change", function() {
+    checkValuesPresence()
+  });
+  $("[name='security[unit_price]']").on("change", function() {
+    checkValuesPresence()
+  });
+  $("[name='security[rate]']").on("change", function() {
+    checkValuesPresence()
+  });
+  $("[name='security[indexer]']").on("change", function() {
+    checkValuesPresence()
+  });
+});
+
+function checkValuesPresence() {
+  issue_date = $("[name='security[issue_date]']").val()
+  maturity = $("[name='security[maturity]']").val()
+  quantity = $("[name='security[quantity]']").val()
+  unit_price = $("[name='security[unit_price]']").val()
+  rate = $("[name='security[rate]']").val()
+  indexer = $("[name='security[indexer]']").val()
+
+  if (issue_date && maturity && quantity && unit_price && rate && indexer) {
+    $.ajax('/securities/ajax_calculate', {
+      method: 'GET',
+      type: 'json',
+      data: {
+        issue_date: issue_date,
+        maturity: maturity,
+        quantity: quantity,
+        unit_price: unit_price,
+        rate: rate,
+        indexer: indexer
+      },
+      success: function(data) {
+        $('#security_current_value').val(data);
+      }
+    })
+  }
+}
+
