9aa1663d4e133ba9576c0d86ac76ed334a657792
Juan Couso <juan.couso@gmail.com>
2017-08-25 12:53:19 -0300

    issues fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ApplicationRecord
   has_many :selling_bids, foreign_key: :seller_id, class_name: "Bid"
   has_many :securities
 
-  validates :name, length: { minimum: 2 }, on: :update
-  validates :cpf, uniqueness: true, format: { with: /[0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2}/i }, on: :update
+  #validates :name, length: { minimum: 2 }, on: :update
+  #validates :cpf, uniqueness: true, format: { with: /[0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2}/i }, on: :update
   # validates :identity, uniqueness: true
-  validates :birthdate, presence: true, date: true, on: :update
-  validates :mother_name, length: { minimum: 2 }, on: :update
-  validates :father_name, length: { minimum: 2 }, on: :update
-  validates :address, presence: true, on: :update
-  validates :phone, uniqueness: true, numericality: { only_integer: true }, on: :update
-  validates :bank, presence: true, on: :update
-  validates :account_agency, uniqueness: true, on: :update
-  validates :account_number, uniqueness: true, on: :update
-  validates :document_type, presence: true, on: :update
-  validates :expedition_date, presence: true, date: true, on: :update
-  validates :document_number, presence: true, uniqueness: true, on: :update
+  #validates :birthdate, presence: true, date: true, on: :update
+  #validates :mother_name, length: { minimum: 2 }, on: :update
+  #validates :father_name, length: { minimum: 2 }, on: :update
+  #validates :address, presence: true, on: :update
+  #validates :phone, uniqueness: true, numericality: { only_integer: true }, on: :update
+  #validates :bank, presence: true, on: :update
+  #validates :account_agency, uniqueness: true, on: :update
+  #validates :account_number, uniqueness: true, on: :update
+  #validates :document_type, presence: true, on: :update
+  #validates :expedition_date, presence: true, date: true, on: :update
+  #validates :document_number, presence: true, uniqueness: true, on: :update
 
   def my_bids_closed
     all = self.securities.map{|s| s.bids }.flatten.select{|b| b.status}
